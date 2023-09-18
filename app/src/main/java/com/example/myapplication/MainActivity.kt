68e421b8d9bbc4916a35c1d9826ef5657dc8e5b8
AdiBhan <abhan03@bu.edu>
2023-09-18 10:55:50 -0400

    Added two input boxes for first and second number entered by user

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var firstNumberInput: EditText
+    lateinit var secondNumberInput: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        firstNumberInput = findViewById(R.id.textInput1)
+        secondNumberInput = findViewById(R.id.textInput2)
     }
+
 }
