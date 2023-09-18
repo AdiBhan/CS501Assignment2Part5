4d0d39eda4ecbd4338fe7e8f9aff753b5265600e
AdiBhan <abhan03@bu.edu>
2023-09-18 11:05:00 -0400

    Added submit button to enter two numbers when you add them together

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,18 +2,24 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var firstNumberInput: EditText
     lateinit var secondNumberInput: EditText
+    lateinit var submitButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         firstNumberInput = findViewById(R.id.textInput1)
         secondNumberInput = findViewById(R.id.textInput2)
+        submitButton = findViewById(R.id.button)
     }
 
+
+
 }
+
