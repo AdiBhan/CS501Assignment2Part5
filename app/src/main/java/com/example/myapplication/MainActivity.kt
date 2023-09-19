bdcbe89d28f6d30f5904201b4f1693fcec6cc1f6
AdiBhan <abhan03@bu.edu>
2023-09-18 21:40:33 -0400

    Updated positioning for submit and inputtext, added radiogroup and radiobuttons. Added handleClick  function which runs when user clicks submit button and checks the operation from RadioButtons and performs the proper logical operation (i.e addition, subtraction, mult), and updates the TextView display to show the result. The function also handles user error i.e dividng by zero

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.myapplication
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
@@ -13,6 +17,16 @@ class MainActivity : AppCompatActivity() {
     lateinit var firstNumberInput: EditText
     lateinit var secondNumberInput: EditText
     lateinit var submitButton: Button
+
+    lateinit var additionRadioButton: RadioButton
+    lateinit var subtractionRadioButton: RadioButton
+    lateinit var multiplicationRadioButton: RadioButton
+    lateinit var dividionRadioButton: RadioButton
+    lateinit var radioGroup: RadioGroup
+    lateinit var displayInit: TextView
+
+    var sum:Int = 0;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,17 +35,61 @@ class MainActivity : AppCompatActivity() {
         secondNumberInput = findViewById(R.id.textInput2)
         submitButton = findViewById(R.id.button)
 
+        additionRadioButton = findViewById(R.id.addition_id)
+        subtractionRadioButton = findViewById(R.id.subtraction_id)
+        multiplicationRadioButton = findViewById(R.id.multiplication_id)
+        dividionRadioButton = findViewById(R.id.division_id)
+        radioGroup = findViewById(R.id.radio_group)
+        displayInit = findViewById(R.id.result_text)
+
+
 
         submitButton.setOnClickListener(View.OnClickListener {
-         Toast.makeText(this,firstNumberInput.text,Toast.LENGTH_SHORT).show()
+            handleCalculate();
+
         })
+
     }
 
+    @SuppressLint("SetTextI18n")
+    fun handleCalculate() {
+        /**
+         * handCalculate function handles the logic for checking the operation
+         * for first and second number and then performing proper operation, and
+         * updating the displayed TextView to show the result.
+         * SideNote: Function also checks to user errors, i.e.
+         * make sure user didn't enter 0 as second number when dividing
+         * */
+        val firstNumber = firstNumberInput.text.toString().toIntOrNull()
+        val secondNumber = secondNumberInput.text.toString().toIntOrNull()
 
+        if (firstNumber != null && secondNumber != null) {
+            when (radioGroup.checkedRadioButtonId) {
+                R.id.addition_id -> {
+                    sum = firstNumber + secondNumber
+                }
+                R.id.subtraction_id -> {
+                    sum = firstNumber - secondNumber
+                }
+                R.id.multiplication_id -> {
+                    sum = firstNumber * secondNumber
+                }
+                R.id.division_id -> {
+                    if (secondNumber != 0) {
+                        sum = firstNumber / secondNumber
+                    } else {
+                        Toast.makeText(this, "Cannot divide by zero! Please change the second number", Toast.LENGTH_SHORT).show()
+                        return  // Exit the function to avoid displaying the result
+                    }
+                }
+            }
+           displayInit.text = "Result of the operation: " + sum.toString()
+        } else {
+            Toast.makeText(this, "Please enter valid numbers", Toast.LENGTH_SHORT).show()
+        }
+        sum = 0
+    }
 
 }
 
-private fun Button.setOnClickListener(mainActivity: MainActivity) {
-    println()
 
-}
